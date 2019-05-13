beb85d8de7f37add15b49725d5fd6ae2bad66302
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-05-13 15:15:42 -0300

    Acrescentada mensagem de Boa Tarde!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,11 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Calendar;
-import java.util.Date;
-
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
-		System.out.println("Hello, World!");		
+		System.out.println("Hello, World!");
+		System.out.println("Good Afternoon!");
 		
 	}
 }
