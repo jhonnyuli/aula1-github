ab4bc187dfae770199c52bdcae6f5b6ae3869920
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-05-13 15:28:16 -0300

    Acrescentada mensagem de Boa Noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 		System.out.println("Hello, World!");
 		System.out.println("Good Afternoon!");
-		System.out.println("Good Night!");	
+		System.out.println("Good Night");
 		
 	}
 }
